3f9a8fdf30241190f23900667a390641fe3d671c
SM <sol@archetyped.com>
2013-12-10 16:11:27 -1000

    Prep for release (0.7.1)

--- main.php
+++ main.php
@@ -3,7 +3,7 @@
 Plugin Name: Cornerstone
 Plugin URI: http://archetyped.com/tools/cornerstone/
 Description: Enhanced content management for Wordpress
-Version: dev (BETA)
+Version: 0.7.1
 Author: Archetyped
 Author URI: http://archetyped.com
 */
